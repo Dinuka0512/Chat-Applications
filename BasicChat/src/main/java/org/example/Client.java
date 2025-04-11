e333637e70aeaeb055fe0267cedf021f179f363e
Dinuka Lakmal <Dinuka0512@gmail.com>
2025-04-11 18:36:47 +0530

    Created the Sercer class

--- BasicChat/src/main/java/org/example/Client.java
+++ BasicChat/src/main/java/org/example/Client.java
@@ -1,4 +1,7 @@
 package org.example;
 
 public class Client {
+    public static void main(String[] args) {
+
+    }
 }
--- BasicChat/src/main/java/org/example/Server.java
+++ BasicChat/src/main/java/org/example/Server.java
@@ -1,7 +1,19 @@
 package org.example;
 
-public class Server {
-    public static void main(String[] args) {
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(5000);
+        System.out.println("Server Started!");
+
+        Socket socket = serverSocket.accept();
+        System.out.println("Client Connected!");
+
+        DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+        System.out.println(dataInputStream.readUTF()); //HERE READ THE CLIENTS MESSAGE
     }
 }
